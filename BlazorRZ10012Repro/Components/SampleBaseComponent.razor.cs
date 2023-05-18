a788c40576475bdfc290de785467891a9d3e8d4f
Michael Stancombe <michael@3cssoftware.com.au>
2023-05-19 08:55:53 +1000

    Add project files.

---
+++ BlazorRZ10012Repro/Components/SampleBaseComponent.razor.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using Microsoft.JSInterop;
+using BlazorRZ10012Repro;
+
+namespace BlazorRZ10012Repro.Components
+{
+    //Uncomment the //: ComponentBase to observe that the RZ10012 warning goes away with a code behind that explicitly inherits from ComponentBase
+    public partial class SampleBaseComponent //: ComponentBase
+    {
+        protected virtual int SomeCount => 5;
+    }
+}
